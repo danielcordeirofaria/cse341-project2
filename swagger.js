96c15e585c92fee63326cd4671e0336ee1875200
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-09-28 15:22:58 -0300

    remouving dev from swagger

--- swagger.js
+++ swagger.js
@@ -1,35 +1,32 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-// Use `cse341-project2-ai9y.onrender.com` como host em produção, ou localhost para desenvolvimento
-const host = process.env.NODE_ENV === 'production' ? 'cse341-project2-ai9y.onrender.com' : 'localhost:3000';
-const schemes = process.env.NODE_ENV === 'production' ? ['https'] : ['http'];
-
 const doc = {
   info: {
     title: 'Library API',
     description: 'API for managing books in a library.',
     version: '1.0.0'
   },
-  host: host,
-  schemes: schemes,
+  host: process.env.SWAGGER_HOST || 'cse341-project2-ai9y.onrender.com',
+  basePath: '/',
+  schemes: ['https'],
   securityDefinitions: {
     google_oauth: {
       type: 'oauth2',
-      authorizationUrl: `${schemes[0]}://${host}/auth/google`,
+      authorizationUrl: `https://${process.env.SWAGGER_HOST || 'cse341-project2-ai9y.onrender.com'}/auth/google`,
       flow: 'implicit',
       scopes: {
-        'read_write': 'Acesso para ler e escrever dados'
+        read_write: 'Acesso para ler e escrever dados'
       }
     }
   },
   security: [{
-    'google_oauth': ['read_write']
+    google_oauth: ['read_write']
   }]
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./routes/index.js', './routes/books.js', './routes/auth.js', './routes/users.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    require('./index.js'); 
+  require('./index.js');
 });
