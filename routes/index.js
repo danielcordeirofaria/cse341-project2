0935e9ce7dab7264abfe1186ef651efcf2ee5fdb
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-09-28 15:49:15 -0300

    alterado localização do arquivo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/auth', require('../auth'));
+router.use('/auth', require('./auth'));
 
 router.use('/books', require('./books'));
 router.use('/users', require('./users'));
